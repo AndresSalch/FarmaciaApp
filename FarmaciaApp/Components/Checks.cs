23a4260eece21a0a58b178e114d643654aa29e4d
salcho <handresalve@gmail.com>
2024-12-20 14:55:32 -0600

    validadores

---
+++ FarmaciaApp/Components/Checks.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FarmaciaApp.Components
+{
+    public class Checks
+    {
+        public bool checkID(string idConsulta)
+        {
+            if (Regex.IsMatch(idConsulta, @"^[1-9]\d{8}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool checkName(string name)
+        {
+            if (Regex.IsMatch(name, "^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\\s]+$\r\n"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool checkEmail(string email)
+        {
+            if (Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public bool checkTel(string tel)
+        {
+            if (Regex.IsMatch(tel, @"^[2687]\d{7}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
